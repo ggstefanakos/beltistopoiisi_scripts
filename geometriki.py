2574d62dc75c4f3898b039975e5eadd01b396bf9
Yorgos <stefanakosgeorg@gmail.com>
2026-01-28 18:22:01 +0200

    i think it calculates one more point than needed

--- geometriki.py
+++ geometriki.py
@@ -37,6 +37,7 @@ def next_int(x):
     else:
         return int(x) + 1
 
+rounding_decs = 3
 n = 2 # Number of dimensions
 a = 0.3 # Error margin
 M_limit = next_int(0.05*n**2 + 1.65*n) # Max number of iterations with the same best value
@@ -44,29 +45,33 @@ M_limit = next_int(0.05*n**2 + 1.65*n) # Max number of iterations with the same 
 start = starting_shape_for_2_dimensions(a)
 # start = np.array([[0.0,0.0],[0.28971,0.07761],[0.07761,0.28971]]) # same as line above
 
+print('i\t|\t\tN\t\t|\tf(n)\t|\trule\t|\tM\t|\t\tR')
+
 current = start
 M = 1
 best_prev = start[0] + np.array([[2000,2000]]) # random huge values that are for sure diferrent than the ines in start
 R_prev = start[0] + np.array([[1000,1000]])
-rule = 1
-i = n + 1
-print('i\t|\t\tN\t\t|\tf(n)\t|\trule\t|\tM\t|\t\tR')
+rule = 0
+i = 1
+
+for x in start:
+    print(f'{i}\t|\t\t-\t\t|\t{np.round(f(x),rounding_decs)}\t|\t-\t|\t-\t|\t-')
+    i += 1
+
 while M < M_limit:
 
     z = [f(x) for x in current] # for minimum
     # z = [-1*f(x) for x in current] # for maximum
 
-    # best = np.min(z)
     best = current[np.where(z == np.min(z))]
 
-    # worst = np.max(z)
     worst = current[np.where(z == np.max(z))]
 
     second_worst = current[np.where(z == np.max(np.delete(z,np.where(z == np.max(z)),axis=0)))]
 
     if (best == best_prev).all():
         M += 1
-        if M >= M_limit: break
+        if M > M_limit: break
     else:
         M = 1
 
@@ -76,19 +81,19 @@ while M < M_limit:
     N = (2/n)*sum_of_all_points - 2*R
     rule = 1
 
-    # N = (2/n)*np.sum(np.delete(current,R)) - R # oxi akribos sosto
-
-    # if (N == R_prev).all(): # kanonas 2
     if np.allclose(N,R_prev): # kanonas 2
         R = second_worst
-        # sum_of_all_points = (current.T @ np.ones((n+1,1))).T
         N = (2/n)*sum_of_all_points - 2*R
         rule = 2
     
+    i += 1
+    print(f'{i}\t|\t{np.round(N,rounding_decs)}\t|\t{np.round(f(N.flatten()),rounding_decs)}\t|\t{rule}\t|\t{M}\t|\t{np.round(R,rounding_decs)}')
+
     R_prev = R
     best_prev = best
     current[np.where(current == R)] = N
-    i += 1
-    print(f'{i}\t|\t{np.round(N,3)}\t|\t{np.round(f(N.flatten()),3)}\t|\t{rule}\t|\t{M}\t|\t{np.round(R,3)}')
 
-print(f'Min of f(x) in x = ')
+    
+
+print(f'\nMin of f(x) in x = ({np.round(best_prev[0,0],rounding_decs)} \u00b1 {a}, {np.round(best_prev[0,1],rounding_decs)} \u00b1 {a})^T')
+print(f'Where f(x) = {np.round(f(best_prev.flatten()),rounding_decs)}')
